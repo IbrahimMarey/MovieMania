efb697b6dc93404dc30be7de285042f270860a80
sobaih12 <153304568+sobaih12@users.noreply.github.com>
2024-02-17 20:14:09 +0200

    search editing

--- app/src/main/java/com/example/movies/home/search/view/SearchFragment.java
+++ app/src/main/java/com/example/movies/home/search/view/SearchFragment.java
@@ -4,15 +4,26 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.movies.R;
+import com.example.movies.home.search.presenter.SearchPresenter;
+import com.example.movies.home.search.presenter.SearchPresenterInterface;
+import com.example.movies.local.MovieLocalSource;
+import com.example.movies.models.pojos.MovieListPojo;
+import com.example.movies.models.repositories.movie.MovieRepo;
+import com.example.movies.remote.MovieConnection;
 
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements SearchFragmentInterface{
 
-
+    SearchPresenterInterface searchPresenterInterface;
+    EditText searchEditText;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -30,6 +41,36 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view;
+        view = inflater.inflate(R.layout.fragment_search, container, false);
+        searchEditText = view.findViewById(R.id.search_edit_text);
+        searchPresenterInterface = new SearchPresenter(this, MovieRepo.getInstance(MovieLocalSource.getInstance(getActivity()), MovieConnection.getInstance()));
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchPresenterInterface.getSearchByMovieName(searchEditText.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onSuccessSearch(MovieListPojo movieListPojo) {
+        Log.i("TAG", "onSuccessSearch: "+movieListPojo.getResults().get(0).getTitle());
+    }
+
+    @Override
+    public void onFailureSearch() {
+
     }
 }
--- app/src/main/java/com/example/movies/home/search/view/SearchFragmentInterface.java
+++ app/src/main/java/com/example/movies/home/search/view/SearchFragmentInterface.java
@@ -1,4 +1,8 @@
 package com.example.movies.home.search.view;
 
+import com.example.movies.models.pojos.MovieListPojo;
+
 public interface SearchFragmentInterface {
+    void onSuccessSearch(MovieListPojo movieListPojo);
+    void onFailureSearch();
 }
