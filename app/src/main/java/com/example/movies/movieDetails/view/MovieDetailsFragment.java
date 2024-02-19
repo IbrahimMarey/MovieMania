cc95b237c09f42c0ee290eab988f81b549ceee9c
AbdullahM98 <58375452+AbdullahM98@users.noreply.github.com>
2024-02-19 17:29:33 +0200

    feat : Movie details fragment created

---
+++ app/src/main/java/com/example/movies/movieDetails/view/MovieDetailsFragment.java
@@ -0,0 +1,32 @@
+package com.example.movies.movieDetails.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.movies.R;
+
+
+public class MovieDetailsFragment extends Fragment {
+
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_details, container, false);
+    }
+}
